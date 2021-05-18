62d81b0629e7b7004e38eaea24e9580d55799615
김가영 <gykim0923@gmail.com>
2021-05-18 23:38:52 +0900

    d

--- src/booksys/application/domain/Menu.java
+++ src/booksys/application/domain/Menu.java
@@ -1,9 +1,13 @@
 package booksys.application.domain;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 public class Menu {
 	
-	  private String menuName ;
-	  
+	 private String menuName ;
+	  HashMap<String, Integer> menuInfo = new HashMap<String, Integer>();
+	
+	 
 	  public Menu (String mn)
 	  {
 	    menuName = mn ;
@@ -13,4 +17,36 @@ public class Menu {
 	  {
 	    return menuName ;
 	  }
+	  public void MenuList() { 
+	  menuInfo.put("불고기피자", 14000);
+
+		menuInfo.put("고르곤졸라", 12000);
+
+		menuInfo.put("로제파스타", 9000);		
+
+		menuInfo.put("토마토파스타", 9000);
+
+		menuInfo.put("크림파스타", 9000);
+
+		
+	  }
+	  Iterator<String> keys = menuInfo.keySet().iterator();
+     
+	  public String chooseMenu(String menuName) { 
+		  while (keys.hasNext()){
+			  String key = keys.next();
+	          if(key.equals(menuName)) { 
+				  return key; } }
+	      }
+	  
+	ArrayList<String> menu= new ArrayList<String>();
+	
+	for(int i =0; i<menu.size(); i++) {
+		menu.addAll(menu);
+}
+		
+
+
+
+}
 }
